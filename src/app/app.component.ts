bb4e3bf5e7138fc21f2bd10bc134eb726314de82
Glenn Gartner <glenngartner@gmail.com>
2017-07-28 14:23:14 -0400

    basic scene rendering, add babylon cube to test import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+﻿import {Component, AfterViewInit, ViewChild} from '@angular/core';
+import {Renderer} from '../renderer/renderer';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
   title = 'app';
+  renderer: Renderer;
+
+  @ViewChild('renderCanvas')
+  canvas: HTMLCanvasElement;
+
+  constructor(){
+    // this.renderer = new Renderer(this.canvas);
+  }
+
+  ngAfterViewInit(){
+    let element = <HTMLCanvasElement>document.getElementById('renderCanvas');
+     this.renderer = new Renderer(element);
+  }
 }
---
+++ src/renderer/renderer.ts
@@ -0,0 +1,36 @@
+﻿import * as BABYLON from 'babylonjs';
+/**
+ * Created by glenn on 7/28/2017.
+ */
+
+export class Renderer {
+  private engine: BABYLON.Engine;
+  private scene: BABYLON.Scene;
+  private camera: BABYLON.Camera;
+
+  constructor(canvas: HTMLCanvasElement){
+    this.engine = new BABYLON.Engine(canvas);
+    this.scene = new BABYLON.Scene(this.engine);
+    this.buildScene(canvas);
+    this.renderLoop(this.engine);
+  }
+
+  buildScene(canvas: HTMLCanvasElement){
+      this.scene.clearColor = new BABYLON.Color4(1, 1, 1, 0);
+      this.camera = new BABYLON.ArcRotateCamera('cam1', 1, .8, 5, new BABYLON.Vector3(0, 0, 0), this.scene);
+      this.camera.attachControl(canvas);
+
+      let light = new BABYLON.DirectionalLight('sunlight', new BABYLON.Vector3(-1, -1, -1), this.scene);
+      light.intensity = .5;
+
+      let box = BABYLON.MeshBuilder.CreateBox('box1', {
+          size: 1
+      }, this.scene);
+  }
+
+  renderLoop(engine: BABYLON.Engine) {
+      engine.runRenderLoop(() => {
+          this.scene.render();
+      })
+  }
+}
