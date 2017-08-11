b9ab8efd084d574c7a3439eacd05fbb6e77acf63
Glenn Gartner <glenngartner@gmail.com>
2017-08-11 00:52:14 -0400

    draw many rows of pixels

--- src/logic/screen/screen-logic.ts
+++ src/logic/screen/screen-logic.ts
@@ -10,7 +10,8 @@ export class ScreenLogic {
 
     constructor(public topLeftCorner: Vector3, public bottomRightCorner: Vector3, public pixelsPerUnit: number, ) {
         this.calculateScreenExtents();
-        this.drawRow();
+        // this.drawRow(this.topLeftCorner.y);
+        this.drawRows();
         this.screenDiagonal = BabylonCommon.euclidianDistanceBetweenVec3Points(topLeftCorner, bottomRightCorner);
         this.calculateScreenExtents();
         this.distBetweenPixels = this.screenExtents.z / pixelsPerUnit;
@@ -26,12 +27,19 @@ export class ScreenLogic {
         this.screenExtents.z = Math.abs(this.topLeftCorner.z - this.bottomRightCorner.z);
     }
 
-    drawRow() {
+    drawRows() {
+        const unitsHigh = this.screenExtents.y * this.pixelsPerUnit;
+        for (let y = this.topLeftCorner.y; y > this.bottomRightCorner.y; y -= this.screenExtents.y / unitsHigh) {
+            console.log('y is: ', y);
+            this.drawRow(y);
+        }
+    }
+
+    drawRow(height: number) {
         const pixelsToDraw = this.screenExtents.z * this.pixelsPerUnit;
-        const startHeight = this.topLeftCorner.y;
         for (let i = 0; i < pixelsToDraw; i ++) {
             const pixel = new Pixel();
-            pixel.position.y = startHeight;
+            pixel.position.y = height;
             pixel.position.z = (this.topLeftCorner.z - i) / this.pixelsPerUnit;
             this.pixelRow.push(pixel);
         }
